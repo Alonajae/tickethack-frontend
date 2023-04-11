928ec059b4c10bbd6365af952ac1aa59328006c6
Veleoran <erwan.mazeas.14.17@gmail.com>
2023-04-11 12:12:04 +0200

    test

---
+++ booking.js
@@ -0,0 +1,16 @@
+async function getBookings() {
+    const response = await fetch("/api/bookings");
+    const bookings = await response.json();
+    displayBookings(bookings);
+  }
+  
+  function displayBookings(bookings) {
+    const bookingsElement = document.getElementById("bookings");
+    bookingsElement.innerHTML = "";
+  }
+  
+    bookings.forEach((booking) => {
+      const bookingElement = document.createElement("div");
+      bookingElement.textContent = `${booking.departure} - ${booking.arrival} 
+  
+    }
