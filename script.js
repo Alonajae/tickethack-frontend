7373122899a286c513d35f2bf69e2c0cc0b84f28
Thomas <thomas.pierreleandre972@gmail.com>
2023-04-12 09:27:55 +0200

    fusion

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-document.getElementById("search-form").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const departure = document.getElementById("departure").value;
-    const arrival = document.getElementById("arrival").value;
-    const date = document.getElementById("date").value;
+flatpickr('.date-picker', {
+    dateFormat: 'd/m/Y'
+});
 
-    const response = await fetch(`/api/trips?departure=${departure}&arrival=${arrival}&date=${date}`);
-    const trips = await response.json();
-
-    displayResults(trips);
+document.querySelector("#search-btn").addEventListener("click", async (e) => {
+   
 });
 
 function displayResults(trips) {
@@ -22,15 +18,24 @@ function displayResults(trips) {
         body: JSON.stringify(data)
 
     })
-    .then(response => response.json())
-    .then(data => {
-        data.forEach((trip) => {
-            const tripElement = document.createElement("div");
-            tripElement.textContent = `${trip.departure} - ${trip.arrival} (${trip.date})`;
-            results.appendChild(tripElement);
-        });
-    })
+        .then(response => response.json())
+        .then(data => {
+            data.forEach((trip) => {
+                const tripElement = document.createElement("div");
+                tripElement.textContent = `${trip.departure} - ${trip.arrival} (${trip.date})`;
+                results.appendChild(tripElement);
+            });
+        })
 }
-flatpickr('.date-picker', {
-    dateFormat: 'd/m/Y'
-});
+
+/*
+ e.preventDefault();
+    const departure = document.getElementById("departure").value;
+    const arrival = document.getElementById("arrival").value;
+    const date = document.getElementById("date").value;
+
+    const response = await fetch(`/api/trips?departure=${departure}&arrival=${arrival}&date=${date}`);
+    const trips = await response.json();
+
+    displayResults(trips);
+*/
